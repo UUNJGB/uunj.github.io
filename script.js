402321abb6cd659293be20b382718a825a2fdda5
UUNJ <jahsonsanders316@gmail.com>
2025-08-21 01:50:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Header glow effect on scroll
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.style.background = "#000022";
+  } else {
+    header.style.background = "#111122";
+  }
+});
 
